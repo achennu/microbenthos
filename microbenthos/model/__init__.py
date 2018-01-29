f262a1fc5c06d8748edab2bfc72d6a9523ee47c2
Arjun Chennu <arjun.chennu@gmail.com>
2018-01-29 13:50:44 +0100

    Added Simulation class
    Simulation class in model/simulate.py
    Tests with mocked Model object
    Simulation parameter checking & start, run_timestep
    Added tests in test_simulate.py

--- microbenthos/model/__init__.py
+++ microbenthos/model/__init__.py
@@ -2,3 +2,4 @@ from .yaml_setup import yaml
 from .loader import from_dict, from_yaml, get_model_schema
 from .model import MicroBenthosModel
 from .saver import save_snapshot
+from .simulate import Simulation
---
+++ tests/test_simulate.py
@@ -0,0 +1,158 @@
+import mock
+import pytest
+
+from microbenthos.model import Simulation
+
+
+@pytest.fixture()
+def model():
+    with mock.patch('microbenthos.model.MicroBenthosModel', autospec=True) as Model:
+        with mock.patch('microbenthos.model.ModelClock', autospec=True) as Clock:
+            model = Model()
+            model.clock = Clock(model)
+            yield model
+
+
+@pytest.fixture()
+def model_with_eqn(model):
+    with mock.patch('fipy.terms.binaryTerm._BinaryTerm', spec=True) as BinaryTerm:
+        model.full_eqn = BinaryTerm()
+        yield model
+
+
+class TestSimulation:
+    def test_init(self):
+        sim = Simulation()
+        assert sim
+        assert sim.started is False
+
+    @pytest.mark.parametrize(
+        'solver', (None,) + Simulation.FIPY_SOLVERS
+        )
+    def test_fipy_solver(self, solver):
+
+        if solver in Simulation.FIPY_SOLVERS:
+            sim = Simulation(fipy_solver=solver)
+            assert sim.fipy_solver == solver
+
+            sim._started = True
+            with pytest.raises(RuntimeError):
+                sim.fipy_solver = solver
+
+        else:
+            with pytest.raises(ValueError):
+                sim = Simulation(fipy_solver=solver)
+
+    @pytest.mark.parametrize(
+        'total,step,error',
+        # hours, seconds, exception
+        [
+            (None, None, ValueError),
+            (1, None, ValueError),
+            (-1, None, ValueError),
+            (1, 3, None),
+            (1, 3600 - 1, None),
+            (1, 3600, ValueError),
+            ]
+        )
+    def test_simtime(self, total, step, error):
+
+        if error:
+            with pytest.raises(error):
+                sim = Simulation(simtime_total=total, simtime_step=step)
+
+        else:
+            sim = Simulation(simtime_total=total, simtime_step=step)
+            assert sim
+
+    @pytest.mark.parametrize('res, error', [
+        (None, ValueError),
+        (2e-6, ValueError),
+        (1e-6, None),
+        (1e-12, None),
+        ]
+                             )
+    def test_residual_lim(self, res, error):
+        if error:
+            with pytest.raises(error):
+                sim = Simulation(residual_lim=res)
+
+        else:
+            sim = Simulation(residual_lim=res)
+            assert sim
+
+    @pytest.mark.parametrize('sweeps, error', [
+        (None, ValueError),
+        (0, ValueError),
+        (1, ValueError),
+        (2, None),
+        (8.5, None),
+        ]
+                             )
+    def test_max_sweeps(self, sweeps, error):
+        if error:
+            with pytest.raises(error):
+                sim = Simulation(max_sweeps=sweeps)
+
+        else:
+            sim = Simulation(max_sweeps=sweeps)
+            assert sim
+
+    def test_set_model(self, model):
+
+        sim = Simulation()
+        with pytest.raises(ValueError):
+            sim.model = None
+        with pytest.raises(ValueError):
+            sim.model = object()
+
+        sim = Simulation()
+
+        with pytest.raises(ValueError):
+            sim.model = model
+            # model.full_eqn is None, so create_full_equation is called once
+            model.create_full_equation.assert_called_once_with(model)
+
+        assert sim.model is None
+        model.full_eqn = object()
+        with pytest.raises(ValueError):
+            sim.model = model
+            # when full_eqn is not a binary term
+
+        with mock.patch('fipy.terms.binaryTerm._BinaryTerm', spec=True) as BinaryTerm:
+            assert sim.model is None
+            model.full_eqn = BinaryTerm()
+            sim.model = model
+            assert sim.model is model
+
+            with pytest.raises(RuntimeError):
+                # because model already set
+                sim.model = model
+
+    def test_start(self, model_with_eqn):
+        sim = Simulation()
+        sim.model = model_with_eqn
+
+        sim.start()
+        assert sim.started
+
+        with pytest.raises(RuntimeError):
+            sim.start()
+
+    def test_run_timestep(self, model_with_eqn):
+        model = model_with_eqn
+        sim = Simulation()
+        sim.model = model
+        sim.start()
+        assert sim.started
+
+        RES = sim.residual_lim/10
+        model.full_eqn.sweep.return_value = RES
+
+        dt = sim.simtime_step
+
+        res = sim.run_timestep()
+        model.update_vars.assert_called_once()
+        model.full_eqn.sweep.assert_called_once()
+        model.clock.increment_time.assert_called_once_with(dt)
+        assert res == RES
