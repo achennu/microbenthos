9b8c6ed984ad16ac09b31e7e66b590e1d5f62287
Arjun Chennu <achennu@mpi-bremen.de>
2018-03-08 23:26:03 +0100

    Import yaml from yaml_setup in utils/__init__.py

--- microbenthos/utils/__init__.py
+++ microbenthos/utils/__init__.py
@@ -1,3 +1,6 @@
 from .create import CreateMixin
 from .loader import validate_dict, validate_yaml, get_schema, find_subclasses_recursive
 from .snapshotters import snapshot_var, restore_var
+from .yaml_setup import yaml
+
+yaml
