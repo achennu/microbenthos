a6ebdaa9e207b58380bf458602d54d72ff8fb120
Arjun Chennu <achennu@mpi-bremen.de>
2019-05-22 13:41:33 +0200

    Bump version: 0.9.2 --> 0.10

--- microbenthos/__init__.py
+++ microbenthos/__init__.py
@@ -3,7 +3,7 @@ from __future__ import division, print_function
 
 __author__ = """Arjun Chennu"""
 __email__ = 'achennu@mpi-bremen.de'
-__version__ = '0.9.2'
+__version__ = '0.10'
 
 import logging
 
